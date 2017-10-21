d72af5a5af25977c633292ea1287ea8c272cfff0
ifala <giacomo@falaschi.com>
2017-10-21 19:42:56 +0200

    main print test

--- main.py
+++ main.py
@@ -1,36 +1,11 @@
 
 # import from folder.file
 #from sensor.distance import sensore_dist_dx
-from sensor.distance import sensore_dist_sx
-#from sensor.photosensor import *
-from sensor.humtemp import temp
-
-
-
-
-#import from lib
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
 
+from sensor.distance import ultrasonic_distance_sensor_high_dx
+from sensor.photosensor import rc_time
 
-class LED_dx(object):
-    pass
-
-
-while 1:
-    if temp() > 50:
-    if sensore_dist_sx() < 50:
-        GPIO.output(LED_dx, 0)
-        GPIO.output(LED_sx, 0)
-        time.sleep(1)
-        GPIO.output(LED_dx, 1)
-    if sensore_dist_sx() < 50:
-        GPIO.output(LED_dx, 0)
-        GPIO.output(LED_sx, 0)
-        time.sleep(1)
-        GPIO.output(LED_sx, 1)
-        noteGPIO.output(LED_dx, 0)
-        GPIO.output(LED_sx, 0)
-        time.sleep(1)
-        GPIO.output(LED_dx, 1)
+print ultrasonic_distance_sensor_high_dx.distance
+print rc_time(7)
