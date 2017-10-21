732e610b160c5000ab1e7e28495d578c48ed3e7f
guidolippi <guidolippi94@hotmail.it>
2017-10-21 22:25:38 +0200

    fix concat

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ try:
     while True:
         luce = rc_time(pin_to_circuit)
         distance = ultrasonic_distance_sensor_high_dx.measure()
-        print (str(ultrasonic_distance_sensor_high_dx.distance) + "  Light value :  " + str(luce))
+        print (str("Distance : %.1f cm" % distance) + "  Light value :  " + str(luce))
         time.sleep(0.5)
 finally:
     GPIO.cleanup()
