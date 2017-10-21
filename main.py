1c33b21136e24ca3eed66f017356c96f30446c1e
ifala <giacomo@falaschi.com>
2017-10-21 19:58:40 +0200

    main print test 4

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 
-from sensor.distance import ultrasonic_distance_sensor_high_dx
+#from sensor.distance import ultrasonic_distance_sensor_high_dx
 from sensor.photosensor import luce
 
-print ultrasonic_distance_sensor_high_dx.distance
+#print ultrasonic_distance_sensor_high_dx.distance
 print luce
 
