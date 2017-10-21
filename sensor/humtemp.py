d141c79b305b4bf84d4105d8e4fb84716e2937b8
ifala <giacomo@falaschi.com>
2017-10-22 00:11:54 +0200

    new dht11 pycode v2

--- sensor/humtemp.py
+++ sensor/humtemp.py
@@ -6,7 +6,7 @@ while True:
 
     humidity, temperature = Adafruit_DHT.read_retry(11, 17)
 
-    print 'Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity)
+    print 'Temp: {0:0.1} C  Humidity: {1:0.1} %'.format(temperature, humidity)
 
 # import RPi.GPIO as GPIO
 # import time
