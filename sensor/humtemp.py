0d78f0eea68493d992eb2716e855ba498663cc31
guidolippi <guidolippi94@hotmail.it>
2017-10-21 18:56:30 +0200

    pin 17 humtemp

--- sensor/humtemp.py
+++ sensor/humtemp.py
@@ -12,16 +12,16 @@ data = []
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4,GPIO.OUT)
-GPIO.output(4,GPIO.HIGH)
+GPIO.setup(17,GPIO.OUT)
+GPIO.output(17,GPIO.HIGH)
 time.sleep(0.025)
-GPIO.output(4,GPIO.LOW)
+GPIO.output(17,GPIO.LOW)
 time.sleep(0.02)
 
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 for i in range(0,500):
-    data.append(GPIO.input(4))
+    data.append(GPIO.input(17))
 
 bit_count = 0
 tmp = 0
