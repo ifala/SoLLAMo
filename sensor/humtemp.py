9ab821a81d6c489e8339465516bbccc8bbc7362b
ifala <giacomo@falaschi.com>
2017-10-21 23:28:01 +0200

    dht11 with adafruit library

--- sensor/humtemp.py
+++ sensor/humtemp.py
@@ -1,19 +1,24 @@
-import RPi.GPIO as GPIO
-import dht11
-
-# initialize GPIO
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-# GPIO.cleanup()
-
-instance = dht11.DHT11(17)
-result = instance.read()
-
-# if result.is_valid():
-#     print("Temperature: %d C" % result.temperature)
-#     print("Humidity: %d %%" % result.humidity)
-# else:
-#     print("Error: %d" % result.error_code)
+# import RPi.GPIO as GPIO
+# import dht11
 #
-print("Temperature: %d C" % result.temperature)
-print("Humidity: %d %%" % result.humidity)
+# # initialize GPIO
+# # GPIO.setwarnings(False)
+# # GPIO.setmode(GPIO.BCM)
+# # # GPIO.cleanup()
+# #
+# # instance = dht11.DHT11(17)
+# # result = instance.read()
+#
+# # if result.is_valid():
+# #     print("Temperature: %d C" % result.temperature)
+# #     print("Humidity: %d %%" % result.humidity)
+# # else:
+# #     print("Error: %d" % result.error_code)
+# #
+# print("Temperature: %d C" % result.temperature)
+# print("Humidity: %d %%" % result.humidity)
+import Adafruit_DHT
+
+while True:
+    humidity, temperature = Adafruit_DHT.read_retry(11, 17)  # GPIO27 (BCM notation)
+    print ("Humidity = {} %; Temperature = {} C".format(humidity, temperature))
