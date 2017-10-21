317358478a8ac2c06e8fc6f69fa71db35937a3b6
guidolippi <guidolippi94@hotmail.it>
2017-10-21 22:03:39 +0200

    fix concat

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 from sensor.distance import ultrasonic_distance_sensor_high_dx
 # from sensor.photosensor import luce
 
-
-print ("test" + str(ultrasonic_distance_sensor_high_dx.distance))
+try:
+    while True:
+        print ("test" + str(ultrasonic_distance_sensor_high_dx.distance))
+finally:
+    GPIO.cleanup()
+    print("exiting main.py")
 
