3b35c680d67ad43a3ef4d6520967f7754d8815f6
ifala <giacomo@falaschi.com>
2017-10-21 19:51:10 +0200

    main print test 2

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 
-# import from folder.file
-#from sensor.distance import sensore_dist_dx
-import RPi.GPIO as GPIO
-import time
 
 from sensor.distance import ultrasonic_distance_sensor_high_dx
-from sensor.photosensor import rc_time
+from sensor.photosensor import rc_time(pin_to_circuit)
 
 print ultrasonic_distance_sensor_high_dx.distance
-print rc_time
+print rc_time(pin_to_circuit)
+
