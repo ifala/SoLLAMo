688b9bb1325d35f8dcb0f065c71f492692ecbce2
guidolippi <guidolippi94@hotmail.it>
2017-10-21 22:15:19 +0200

    fix concat

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 
 import RPi.GPIO as GPIO
+import time
 from sensor.distance import ultrasonic_distance_sensor_high_dx
 from sensor.photosensor import luce
 
 try:
     while True:
         print (str(ultrasonic_distance_sensor_high_dx.distance) + "  Light value :  " + str(luce))
-        sleep(0.5)
+        time.sleep(0.5)
 finally:
     GPIO.cleanup()
     print("exiting main.py")
