1f2604ee364064586062e46f0df45548de43cb11
guidolippi <guidolippi94@hotmail.it>
2017-10-24 19:02:45 +0200

    Trying fix main with various import

--- main.py
+++ main.py
@@ -1,23 +1,22 @@
 
 import RPi.GPIO as GPIO
-# import time
-# from sensor.distance import ultrasonic_distance_sensor_high_dx
-# from sensor.photosensor import pin_to_circuit, rc_time
+import time
+from sensor.distance import ultrasonic_distance_sensor_high_dx
+from sensor.photosensor import pin_to_circuit, rc_time
 
 # todo funziona bo
 #distance = ultrasonic_distance_sensor_high_dx.measure()
 #luce = rc_time(pin_to_circuit)
 
-# try:
-#     while True:
-#         distance = ultrasonic_distance_sensor_high_dx.measure()
-#         luce = rc_time(pin_to_circuit)
-#         print ("Distance : %.1f cm" % distance)
-#         print ("Light value :  " + str(luce))
-#         print (" ")
-#         time.sleep(3.5)
-# finally:
-#     GPIO.cleanup()
-#     print("exiting main.py")
+try:
+    while True:
+        distance = ultrasonic_distance_sensor_high_dx.measure()
+        luce = rc_time(pin_to_circuit)
+        print("Distance : %.1f cm" % distance)
+        print("Light value :  " + str(luce))
+        print(" ")
+        time.sleep(1)
+finally:
+    GPIO.cleanup()
+    print("exiting main.py")
 
-print("Hola this is a test")
