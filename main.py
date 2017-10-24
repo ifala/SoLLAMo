5bfa574c722b71e4bb234fe5175fce50ebc84c1f
guidolippi <guidolippi94@hotmail.it>
2017-10-24 19:40:48 +0200

    fake analog class and new photosensor

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 import RPi.GPIO as GPIO
 import time
 from sensor.distance import ultrasonic_distance_sensor_high_dx
-from sensor.photosensor_OO import photosensor
+# from sensor.photosensor_OO import photosensor
+
+from sensor.fake_analog_input_rbpi import FakeAnalogInput
 
 # from sensor.photosensor import pin_to_circuit, rc_time
 
@@ -12,11 +14,12 @@ from sensor.photosensor_OO import photosensor
 
 try:
     while True:
+        photosensor = FakeAnalogInput(4)
         distance = ultrasonic_distance_sensor_high_dx.measure()
         # luce = rc_time(pin_to_circuit)
-        # light = photosensor.read_fake_analog_value()
+        light = photosensor.read_fake_analog_value()
         print("Light level revelated is: ")
-        # print(light)
+        print(light)
 
         print(" Distance : %.1f cm" % distance)
         # print("Light value :  " + str(luce))
